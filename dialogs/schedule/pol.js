ac434856e7d38aae4f1cf210fe851c93deabf083
Alyssa Ong <alyssaong@gmail.com>
2018-05-29 15:34:35 +0900

    Added pol handling

--- dialogs/schedule/pol.js
+++ dialogs/schedule/pol.js
@@ -1,3 +1,18 @@
-module.exports = function(session) {
-    return session.beginDialog('Card');
-}
+module.exports = [
+    function(session) {
+        session.beginDialog('Card');
+    },
+    function(session, results) {
+        if (!results.inputpolval) {
+            session.send('No POL entered. Please enter a POL.')
+            // TODO: populate with existing values
+            session.message.value = null;
+            session.clearDialogStack();
+            session.beginDialog('POL/POD Inquiry')
+        } else {
+            // Handle results
+            console.log(results);
+            session.endDialog('입력해주셔서 감사합니다. 홈 화면으로 돌아갑니다');
+        }
+    }
+];
